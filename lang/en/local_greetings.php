26975a18b8cc173057bc36a5e75651ee810a75df
Rajneel Totaram <rajneel.totaram@moodle.com>
2023-04-17 12:13:37 +1200

    Add new capabilities

--- lang/en/local_greetings.php
+++ lang/en/local_greetings.php
@@ -26,6 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['greetingloggedinuser'] = 'Greetings, {$a}.';
+$string['greetings:postmessages'] = 'Post a new message on the Greetings wall';
+$string['greetings:viewmessages'] = 'View messages on the Greetings wall';
 $string['greetinguser'] = 'Greetings, user.';
 $string['greetinguserau'] = 'Hello, {$a}.';
 $string['greetinguseres'] = 'Hola, {$a}.';
