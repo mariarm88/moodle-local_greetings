3ccf5a5491952ab35f67a7ec32696199e0bb7f22
Rajneel Totaram <rajneel.totaram@moodle.com>
2024-07-31 10:06:05 +1200

    Use pluginname for title

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $context = context_system::instance();
 $PAGE->set_context($context);
 $PAGE->set_url(new moodle_url('/local/greetings/index.php'));
 $PAGE->set_pagelayout('standard');
-$PAGE->set_title($SITE->fullname);
+$PAGE->set_title(get_string('pluginname', 'local_greetings'));
 $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 
 require_login();
