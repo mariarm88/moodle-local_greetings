5110ffaa815c3f50e4d95d8df7e41abace5dd88d
Rajneel Totaram <rajneel.totaram@moodle.com>
2025-03-28 19:48:33 +1200

    Sort lang strings

--- lang/en/local_greetings.php
+++ lang/en/local_greetings.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Greetings';
-$string['greetinguser'] = 'Greetings, user.';
 $string['greetingloggedinuser'] = 'Greetings, {$a}.';
+$string['greetinguser'] = 'Greetings, user.';
 $string['greetinguserau'] = 'Hello, {$a}.';
 $string['greetinguseres'] = 'Hola, {$a}.';
 $string['greetinguserfj'] = 'Bula, {$a}.';
 $string['greetingusernz'] = 'Kia Ora, {$a}.';
+$string['pluginname'] = 'Greetings';
